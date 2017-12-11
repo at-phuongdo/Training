5d1a06adbbcb560939cc763fcf7ce797388443a4
at-phuongdo <dophuong13t2@gmail.com>
2017-12-11 08:40:30 +0700

    Change color header

--- Exercise/javascipt.js
+++ Exercise/javascipt.js
@@ -1,17 +1,10 @@
 $(document).ready(function(){
     var setHeight = $(".main-top-left").height();
-    console.log(setHeight);
     $('.tabcontent').css({'height': setHeight});
 });
 
-// var setHeight = document.getElementById('main-top-left').clientHeight;
-// document.getElementsByClassName('tabcontent').style.height  = setHeight +"px";
-
-//lấy các thẻ a đại diện cho các tab
 var buttons = document.getElementsByClassName('tablinks');
-//lấy các phần nội dung
 var contents = document.getElementsByClassName('tabcontent');
-//Định nghĩa hàm hiển thị nội dung theo id
 function showContent(id){
     for (var i = 0; i < contents.length; i++) {
         contents[i].style.display = 'none';
@@ -19,23 +12,15 @@ function showContent(id){
     var content = document.getElementById(id);
     content.style.display = 'block';
 }
-//lặp qua các tab và gán sự kiện click
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(){
-        //lấy văn bản trong thẻ a đại diện cho id của nội dung
         var id = this.textContent;
-        //bỏ active tất cả các tab
         for (var i = 0; i < buttons.length; i++) {
             buttons[i].classList.remove("active");
         }
-    //active tab được click
     this.className += " active";
-    //show nội dung theo id lấy được
-    
-    console.log(id);
     showContent(id);
 });
 }
-//mặc định hiển thị tab HTML
 showContent('HTML');
 
