552190c9154536a5a6c0d1dab60f5f753c179f78
at-phuongdo <dophuong13t2@gmail.com>
2018-01-23 16:56:21 +0700

    vuex

---
+++ vuex/vuex-playlist/src/store/store.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    products: [
+    {name:"Banana", price:20},
+    {name:"Apple", price:40},
+    {name:"Green Shells", price:60},
+    {name:"Red Shells", price:40},
+    {name:"Shiny Star", price:20},
+    ]
+  },
+  // Truy cap du lieu state, filter
+  getters: {
+    saleProducts:state => {
+      var saleProducts = state.products.map(product => {
+        return {
+          name:'**' + product.name + '**',
+          price: product.price/2
+        }
+      });
+      return saleProducts;
+    }
+  },
+  // Lam thay doi du lieu state
+  mutations: {
+    reducePrice:state => {
+      state.products.forEach( product => {
+        product.price -= 1
+      });
+    }
+  },
+  actions: {
+    reducePrice: context => {
+      setTimeout(function(){
+        context.commit('reducePrice')
+      },2000)
+    }
+  }
+})
